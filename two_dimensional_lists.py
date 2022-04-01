d4522ddc74e4242a0aa9cc1c75da5acd07179646
Abhijeet Kumar <abhijeetkumar_1992@yahoo.in>
2022-04-02 01:06:08 +0530

    Initializing a 2D array

--- two_dimensional_lists.py
+++ two_dimensional_lists.py
@@ -1,11 +1,6 @@
-a = 'This is a line'
+a = [[0, 1], [2, 3]]
+print(a[0][1])
 
-print('Get string by index')
-print(a[0: 4])
-
-print('Split string into list')
-b = a.split()
-print(b)
 
 # To initialize an list of length 5
 a = [0] * 5
@@ -18,4 +13,3 @@ print(a)
 # To initialize a two-dimensional list with size 5 X 5
 a = [[0]*5] * 5
 print(a)
-
